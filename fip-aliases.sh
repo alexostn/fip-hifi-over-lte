eba9b6002027f37d29f0ed462f70512f64b33aa2
oostapen <157902890+alexostn@users.noreply.github.com>
2026-04-08 01:00:17 +0200

    ٩(◕‿◕)~~~*✲  add FIP aliases for music genres
    This script defines aliases for various music genres using the fip-stream.sh script.

---
+++ fip-aliases.sh
@@ -0,0 +1,19 @@
+fip-aliases
+# ٩(◕‿◕)~*✲ FIP aliases — add to ~/.bashrc or ~/.zshrc
+# source this file: echo 'source ~/fip-hifi-over-lte/fip-aliases.sh' >> ~/.bashrc
+
+FIP_SCRIPT="${HOME}/fip-hifi-over-lte/fip-stream.sh"
+
+alias fip="$FIP_SCRIPT"
+alias fjazz="$FIP_SCRIPT jazz"
+alias frock="$FIP_SCRIPT rock"
+alias fgroove="$FIP_SCRIPT groove"
+alias fworld="$FIP_SCRIPT world"
+alias felectro="$FIP_SCRIPT electro"
+alias fhip="$FIP_SCRIPT hiphop"
+alias fpop="$FIP_SCRIPT pop"
+alias fmetal="$FIP_SCRIPT metal"
+alias freggae="$FIP_SCRIPT reggae"
+alias fnouveau="$FIP_SCRIPT nouveautes"
+alias fsacre="$FIP_SCRIPT sacre"
+alias fcultes="$FIP_SCRIPT cultes"
